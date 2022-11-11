38b40065db9c9529c8d8777d3180446bb02ce81c
joaobijos <joaopedrobijos96@gmail.com>
2022-11-10 22:54:58 -0300

    Creating score

--- js/script.js
+++ js/script.js
@@ -2,22 +2,33 @@
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
 const clouds = document.querySelector('.clouds')
+const clouds2 = document.querySelector('.clouds2')
+const lose = document.querySelector('.title')
 
+var score = 1;
 
 const jump = () => {
+     
     mario.classList.add('jump');
-
+    
     setTimeout(() => {
         mario.classList.remove('jump');
     }, 500);
-}
+
+    var placar = score++; 
+    document.getElementById("points").innerHTML = placar;
+    document.getElementById("placarFinal").innerHTML = placar;
+ }
+
+document.addEventListener('keydown', jump);
+
 
 const loop = setInterval(()=> {
 
     const pipePosition = pipe.offsetLeft;
     const cloudsPosition = clouds.offsetLeft;
+    const cloudsPosition2 = clouds2.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-
 
     if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
 
@@ -27,6 +38,9 @@ const loop = setInterval(()=> {
         clouds.style.animation = 'none';
         clouds.style.left = `${cloudsPosition}px`
 
+        clouds2.style.animation = 'none';
+        clouds2.style.left = `${cloudsPosition2}px`
+
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
@@ -34,9 +48,10 @@ const loop = setInterval(()=> {
         mario.style.width= '75px';
         mario.style.marginLeft= '50px';
 
+        lose.style.display= 'flex'
+
         clearInterval(loop);
     }
 
 }, 10);
 
-document.addEventListener('keydown', jump);
